e9818e304009ab7a8f934d1bc82c5adbf9d2936f
Maicon Bispo <emaildomaicon@icloud.com>
2025-12-12 18:52:54 -0300

    Revert viabilidade filter title

--- contratos/admin.py
+++ contratos/admin.py
@@ -338,7 +338,7 @@ class PrescricaoOrderFilter(admin.SimpleListFilter):
 
 
 class ViabilidadeFinanceiraFilter(admin.SimpleListFilter):
-    title = "Viabilidade"
+    title = "Viabilidade $"
     parameter_name = 'viabilidade_financeira'
 
     def lookups(self, request, model_admin):
